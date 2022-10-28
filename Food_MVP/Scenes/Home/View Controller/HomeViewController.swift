d9aea4c223f20ad3b7ca349adb3f56de1c2266d6
Donia Elshenawy <doniaelshenawyali@gmail.com>
2022-10-29 01:40:49 +0200

    add code time of simulator in home screen.

--- Food_MVP/Scenes/Home/View Controller/HomeViewController.swift
+++ Food_MVP/Scenes/Home/View Controller/HomeViewController.swift
@@ -10,51 +10,52 @@ import UIKit
 class HomeViewController: UIViewController {
 
 
-    @IBOutlet weak var searchTabBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
     var presenter: HomeVCPresenter?
     var interactor: FoodInteractor!
     var router: HomeVCRouter!
     
+    let searchController = UISearchController()
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(named: "Color")]
         navigationItem.setHidesBackButton(true, animated: true)
         navigationItem.backButtonTitle = ""
-
-
-
-       // title = "Good morning"
+        title = "Good morning"
         setDataTableView()
         presenter?.viewDidLoad()
 
-//        let today = Date()
-//        // 2. Pick the date components
-//        let hours   = (Calendar.current.component(.hour, from: today))
-//        let minutes = (Calendar.current.component(.minute, from: today))
-//        let seconds = (Calendar.current.component(.second, from: today))
-//        // 3. Show the time
-//        print("\(hours):\(minutes)")
+        navigationItem.searchController = searchController
         
-        let currentDateTime = Date()
-
-        // initialize the date formatter and set the style
-        let formatter = DateFormatter()
-        // "10:52:30 PM"
-        formatter.timeStyle = .medium
-        formatter.dateStyle = .none
-        let t = formatter.string(from: currentDateTime)
+       
+        let today = Date()
+        // 2. Pick the date components
+        let hours   = (Calendar.current.component(.hour, from: today))
+        let minutes = (Calendar.current.component(.minute, from: today))
+        let seconds = (Calendar.current.component(.second, from: today))
+        // 3. Show the time
+        print("\(hours):\(minutes)")
         
-        for i in t {
-            if i == "A"{
-                print("a")
-            }
-            else if i == "p"{
-                print("pp")
-            }
-        }
+//        let currentDateTime = Date()
+//
+//
+//        // initialize the date formatter and set the style
+//        let formatter = DateFormatter()
+//        // "10:52:30 PM"
+//        formatter.timeStyle = .medium
+//        formatter.dateStyle = .none
+//        let t = formatter.string(from: currentDateTime)
+//
+//        for i in t {
+//            if i == "A"{
+//                print("a")
+//            }
+//            else if i == "p"{
+//                print("pp")
+//            }
+//        }
        
 
 
